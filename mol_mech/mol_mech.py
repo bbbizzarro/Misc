439fc113e8b60c0bf1546542e547315e6c7ffe36
Brandon Bizzarro <bbbizzarro@gmail.com>
2019-03-24 16:35:04 -0700

    began molecular mechanics sim

---
+++ mol_mech/mol_mech.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+def elec():
+
+class MonSys:    
+    def __init__(self):
+        
+        
+
+def main():
+    continue
+         
+if __name__ == '__main__':
+    main()
